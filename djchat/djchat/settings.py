6b786f289f4d0f6fabc55a8fcd2d442c17d916a6
KOOPAKO <cooperearps1@gmail.com>
2023-07-11 16:18:06 +1000

    Development of chat server admin api.

--- djchat/djchat/settings.py
+++ djchat/djchat/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # External
     "drf_spectacular",
+    "rest_framework",
     # Internal
     "server",
     "account",
@@ -114,6 +115,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -124,6 +127,7 @@ AUTH_USER_MODEL = "account.Account"
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_AUTHENTICATION_CLASSES": ["rest_framework.authentication.SessionAuthentication"],
 }
 
 SPECTACULAR_SETTINGS = {
