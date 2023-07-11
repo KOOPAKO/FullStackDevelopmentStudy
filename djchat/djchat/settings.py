33a6795a07f9ac8adef2fc6cd3622ff83b53b615
KOOPAKO <cooperearps1@gmail.com>
2023-07-11 12:50:56 +1000

    DRF project initiation complete. And started admin API.

--- djchat/djchat/settings.py
+++ djchat/djchat/settings.py
@@ -30,6 +30,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # External
+    "drf_spectacular",
+    # Internal
+    "server",
+    "account",
 ]
 
 MIDDLEWARE = [
@@ -114,3 +119,16 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "account.Account"
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "DjChat API",
+    "DESCRIPTION": "API for DjChat",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": True,
+}
